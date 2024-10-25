413cefcb67fd200066b750cbebdbb1998cf377ca
Divyam Sinha <divyamsinha@Divyams-MacBook-Air.local>
2024-10-25 11:54:16 +0530

    Checking of the page content

--- App.js
+++
@@ -1,5 +0,0 @@
-const element = React.createElement('h1', {className: "heading"}, React.createElement('div', { className: "child"}, 'studentsss'))
-
-const reactRoot = ReactDOM.createRoot(document.getElementById('root'))
-
-reactRoot.render(element)
